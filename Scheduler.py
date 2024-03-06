67a115aef72eeb493e618fced9d42155f8d974f4
Ahmed ElZubair <ae87@hw.ac.uk>
2024-03-06 16:05:20 +0400

    pushing

--- Scheduler.py
+++ Scheduler.py
@@ -156,6 +156,7 @@ import numpy
 class Scheduler:
 
     def __init__(self, production_schedule_df, rm_availability_df, blend_stock_availability_df, consumption_rates_efficiency_df, transfer_time_matrix):
+
         self.production_schedule_df = production_schedule_df
         self.rm_availability_df = rm_availability_df
         self.blend_stock_availability_df = blend_stock_availability_df
@@ -170,6 +171,7 @@ class Scheduler:
         self._initialize_ga()
 
     def _initialize_entities(self):
+
         for index, row in updated_parameters_df.iterrows():
             self.silos[row['Name']] = Silo(row['Name'], row['Maximum Capacity (kg)'], row['Transfer Type'])
 
@@ -190,6 +192,7 @@ class Scheduler:
                 self.silos[silo_name].add_stock(blend_code, quantity)
 
     def _initialize_ga(self):
+
         creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
         creator.create("Individual", list, fitness=creator.FitnessMin)
 
@@ -206,9 +209,10 @@ class Scheduler:
     def evaluate_individual(self, individual):
         # This is where you translate the individual (a list of task indices) into a scheduling decision
         # For simplicity, this is a placeholder. You need to implement this based on your project specifics
-        return (1.0,)  # Dummy fitness value
+        return (6.0,)  # Dummy fitness value
 
     def optimize_schedule_with_ga(self):
+        
         pop = self.toolbox.population(n=300)
         CXPB, MUTPB, NGEN = 0.5, 0.2, 40
 
@@ -226,6 +230,7 @@ class Scheduler:
         # Additional logic to apply the best scheduling solution to your system
 
     def decode_individual_to_schedule(self, individual):
+
         decoded_tasks = [self.production_schedule_df.iloc[idx] for idx in individual]
         # Sort or prioritize tasks based on youpusr criteria, for example, by 'Due Date'
         sorted_tasks = sorted(decoded_tasks, key=lambda x: x['Due Date'])
@@ -243,6 +248,7 @@ class Scheduler:
         return schedule
     
     def print_schedule(self, schedule):
+
         for line_name, tasks in schedule.items():
             print(f"Schedule for {line_name}:")
             for task in tasks:
